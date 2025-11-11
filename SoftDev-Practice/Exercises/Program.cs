ef5ba83c66e5f787f8f4938fd74f4d9d49f7fcb8
HernandoNJ <hernandonj.unity@gmail.com>
2025-11-11 15:39:24 -0500

    5 #s, 5 rows

---
+++ SoftDev-Practice/Exercises/Program.cs
@@ -0,0 +1,16 @@
+﻿// #####
+// #####
+// #####
+// #####
+// #####
+
+for (int row = 1; row <= 5; row++)
+{
+    for (int col = 1; col <= 5; col++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
